a6de0167e063b1db3ee7ad2e382bebd48f75af66
Péter Pallos <pallosp@gmail.com>
2026-03-10 09:10:49 +0100

    Fix the logo's click target

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -12,17 +12,15 @@ export function Logo({ variant }: LogoProps) {
     variant === 'white' ? '/images/logo-white.svg' : '/images/logo-black.svg';
 
   return (
-    <div className={styles.container}>
-      <Link href="/">
-        <Image
-          src={logoSrc}
-          alt="Logo"
-          className={styles.logo}
-          width={60}
-          height={78}
-          priority
-        />
-      </Link>
-    </div>
+    <Link className={styles.link} href="/">
+      <Image
+        src={logoSrc}
+        alt="Logo"
+        className={styles.logo}
+        width={60}
+        height={78}
+        priority
+      />
+    </Link>
   );
 }
