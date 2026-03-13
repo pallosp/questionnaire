fdd9e569a0e5b806b5b6b3a84036c87ff232beea
Péter Pallos <pallosp@gmail.com>
2026-03-13 10:21:51 +0100

    Serve the background image as WebP

--- src/app/questionnaire/[id]/[questionNumber]/background.tsx
+++ src/app/questionnaire/[id]/[questionNumber]/background.tsx
@@ -1,8 +1,10 @@
+import { getImageProps } from 'next/image';
+
 import { BASE_PATH } from '@/lib/config-utils';
 
 import styles from './background.module.css';
 
-interface BackgroundProps {
+export interface BackgroundProps {
   // Background image file name prefix, e.g. 'a3'.
   // TODO: Is it guaranteed to be valid?
   variant: string;
@@ -10,31 +12,60 @@ interface BackgroundProps {
 
 export function Background({ variant }: BackgroundProps) {
   const pathPrefix = `${BASE_PATH}/images/${variant}`;
+
+  const common = { alt: '', fill: true, priority: true };
+
+  const desktop = getImageProps({
+    ...common,
+    src: `${pathPrefix}_d.jpg`,
+  }).props;
+  const tabletL = getImageProps({
+    ...common,
+    src: `${pathPrefix}_t_l.jpg`,
+  }).props;
+  const tabletP = getImageProps({
+    ...common,
+    src: `${pathPrefix}_t_p.jpg`,
+  }).props;
+  const mobileL = getImageProps({
+    ...common,
+    src: `${pathPrefix}_m_l.jpg`,
+  }).props;
+  const mobileP = getImageProps({
+    ...common,
+    src: `${pathPrefix}_m_p.jpg`,
+  }).props;
+
   return (
     <div className={styles.container}>
       <picture>
         {/* tablet landscape */}
         <source
           media="(768px <= width < 1280px) and (orientation: landscape)"
-          srcSet={`${pathPrefix}_t_l.jpg`}
+          srcSet={tabletL.srcSet}
         />
         {/* tablet portrait */}
         <source
           media="(768px <= width < 1280px) and (orientation: portrait)"
-          srcSet={`${pathPrefix}_t_p.jpg`}
+          srcSet={tabletP.srcSet}
         />
         {/* mobile landscape */}
         <source
           media="(width < 768px) and (orientation: landscape)"
-          srcSet={`${pathPrefix}_m_l.jpg`}
+          srcSet={mobileL.srcSet}
         />
         {/* mobile portrait */}
         <source
           media="(width < 768px) and (orientation: portrait)"
-          srcSet={`${pathPrefix}_m_p.jpg`}
+          srcSet={mobileP.srcSet}
         />
         {/* desktop / fallback */}
-        <img src={`${pathPrefix}_d.jpg`} alt="" className={styles.image} />
+        <img
+          src={desktop.src}
+          srcSet={desktop.srcSet}
+          alt=""
+          className={styles.image}
+        />
       </picture>
       <div className={styles.overlay} />
     </div>
